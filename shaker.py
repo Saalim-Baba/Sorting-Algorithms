f8b4a163728b92e65b3ec8b68ec9447e065d9031
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-03-27 14:32:28 +0100

    load data modified

--- shaker.py
+++ shaker.py
@@ -1,8 +1,20 @@
+import datetime
+
+
 def load_data():
     subarray = []
-    with open("SortSmall.txt", "r") as file:
+    with open("SortBig.txt", "r") as file:
         big_array = file.read().split("\n")
         for entry in big_array:
-            subarray.append(entry.split(","))
+            entry = entry.split(",")
+            subarray.append(entry)
+            for i in subarray:
+                try:
+                    i[0] = int(i[0])
+                    i[4] = int(i[4])
+                    if isinstance(i[5], str):
+                        i[5] = datetime.datetime(int(i[5].split(".")[2]), int(i[5].split(".")[1]),int(i[5].split(".")[0]))
+                    i[6] = float(i[6])
+                except ValueError:
+                    continue
         return subarray
-
