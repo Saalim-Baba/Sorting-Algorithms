2bdd8b8094f391890697af178bdc79915a7d35e4
Poisonlocket <loribono@gmx.ch>
2024-03-27 14:33:17 +0100

    updating repo

--- bubblesort.py
+++ bubblesort.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def load_data():
     subarray = []
-    with open("SortSmall.txt", "r") as file:
+    with open("SortMedium.txt", "r") as file:
         big_array = file.read().split("\n")
         for entry in big_array:
             entry = entry.split(",")
@@ -12,9 +12,11 @@ def load_data():
                 try:
                     i[0] = int(i[0])
                     i[4] = int(i[4])
-
+                    if isinstance(i[5], str):
+                        i[5] = datetime(int(i[5].split(".")[2]), int(i[5].split(".")[1]),
+                                                 int(i[5].split(".")[0]))
                     i[6] = float(i[6])
-                    print(i)
+
                 except ValueError:
                     continue
         return subarray
@@ -27,8 +29,7 @@ def bubblesort_int(data: list, pos_count: int):
     datalength = len(data)-1
     for iteration in range(datalength):
         for element in range(datalength-iteration):
-            print(data[element])
-            print(element)
+
             if data[element][pos_count] > data[element+1][pos_count]:
                 data[element], data[element + 1] = data[element + 1], data[element]
             else:
