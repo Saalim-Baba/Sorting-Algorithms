f5bfc93bd7b98d317d79c5c6f9a2e24c35c778d5
Poisonlocket <loribono@gmx.ch>
2024-03-27 14:10:08 +0100

    setup project, implemented bubble sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,7 +1,40 @@
+from datetime import datetime
+
+
 def load_data():
     subarray = []
     with open("SortSmall.txt", "r") as file:
         big_array = file.read().split("\n")
         for entry in big_array:
-            subarray.append(entry.split(","))
+            entry = entry.split(",")
+            subarray.append(entry)
+            for i in subarray:
+                try:
+                    i[0] = int(i[0])
+                    i[4] = int(i[4])
+
+                    i[6] = float(i[6])
+                    print(i)
+                except ValueError:
+                    continue
         return subarray
+
+
+
+
+
+def bubblesort_int(data: list, pos_count: int):
+    datalength = len(data)-1
+    for iteration in range(datalength):
+        for element in range(datalength-iteration):
+            print(data[element])
+            print(element)
+            if data[element][pos_count] > data[element+1][pos_count]:
+                data[element], data[element + 1] = data[element + 1], data[element]
+            else:
+                continue
+
+    return data
+
+print(bubblesort_int(load_data(), 4))
+
--- quicksort.py
+++ quicksort.py
@@ -1,6 +1,6 @@
-def load_data():
+def load_data(filepath:str):
     subarray = []
-    with open("SortSmall.txt", "r") as file:
+    with open(filepath, "r") as file:
         big_array = file.read().split("\n")
         for entry in big_array:
             subarray.append(entry.split(","))
